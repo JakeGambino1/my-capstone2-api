4076d3f9c878ed3cdabe15c50f091d6abad229ea
Jacob Gambino <marketingboon@jacobs-mbp.home>
2019-11-06 13:58:43 -0600

    added user model

---
+++ schema/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  isMentor: {
+    type: Boolean
+  },
+  isAdmin: {
+    type: Boolean
+  },
+  requestsMentorship: {
+    type: Boolean
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
